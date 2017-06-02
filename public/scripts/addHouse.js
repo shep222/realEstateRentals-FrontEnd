c5b062201e88a09bfd6dd5d8dc56f6ea79f4181b
John Shepler <john.sheplerjr@gmail.com>
2017-06-02 09:42:10 -0600

    back to 3 posts

--- public/scripts/addHouse.js
+++ public/scripts/addHouse.js
@@ -1,25 +1,49 @@
 $(() => {
-  $('form').submit(uploadImage)
+    $('form').submit(createNewHouse)
 
 })
 
-function uploadImage(event) {
-  event.preventDefault()
-  let img = $('input[type=file]').prop('files')[0]
-  let formData = new FormData()
-  formData.append("image", img)
-  console.log(formData);
-  $.ajax({
-    url: 'https://realestate-rentals.herokuapp.com/image',
-    data: formData,
-    processData: false,
-    contentType: false,
-    type: 'POST',
-    success: data => {
-      console.log('IT DID IT');
-    },
-    fail: error => {
-      console.log(error);
-    }
-  })
+function createNewHouse(event) {
+    event.preventDefault()
+    // let img = $('input[type=file]').prop('files')[0]
+    // let formData = new FormData()
+    // formData.append("image", img)
+    var newHouse = {};
+    newHouse.address = $('.newAddress').val()
+    newHouse.city = $('.newCity').val()
+    newHouse.state = $('.newState').val()
+    newHouse.zipcode = $('.newZipcode').val()
+    newHouse.bedrooms = $('.newBedrooms').val()
+    newHouse.baths = $('.newBaths').val()
+    newHouse.available = $('.newAvailable').val()
+    newHouse.description = $('.newDescription').val()
+
+    $.ajax({
+        url: 'https://realestate-rentals.herokuapp.com/house',
+        method: 'POST',
+        crossDomain: true,
+        data: JSON.stringify(newHouse),
+        contentType: "application/json; charset=utf-8"
+    })
+    .then(response => {
+        console.log(response);
+        window.location.reload()
+    }).catch(err => {
+        console.log(err);
+    })
 }
+
+// $.ajax({
+//     url: 'https://realestate-rentals.herokuapp.com/image',
+//     data: formData,
+//     processData: false,
+//     contentType: false,
+//     type: 'POST',
+//     success: data => {
+//         console.log(data);
+//     },
+//     fail: error => {
+//         console.log(error);
+//     }
+// })
+// }
